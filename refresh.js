1dfd9700f28a55206b529653ce1a592c365fc12a
KissaVasara <catreapy@gmail.com>
2022-10-14 12:07:58 +0300

    init

---
+++ refresh.js
@@ -0,0 +1,5 @@
+function refresh(){ 
+window.location = window.location
+}
+/*Refreshes the page every 20 minutes to prevent auto log off. My dumbass couldn't find a better way.*/
+setInterval(refresh, 1200000)
