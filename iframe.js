3747f4815a00180ba6302504d0c7725542806f20
aaroluo <aaro.luo@gmail.com>
2022-11-18 13:48:28 +0200

    timeout hack

--- iframe.js
+++ iframe.js
@@ -2,25 +2,24 @@ const iframeCSS = `
 html,body {
   background-color: transparent !important; 
   color: white !important;
-}
-`
+}`
 
-console.log("run inject");
 document.querySelectorAll("iframe").forEach(iframe => {
 
-  console.log("inject iframe");
   const ws = document.getElementById("WilmaStyles")
   let brightness =
     getComputedStyle(ws).getPropertyValue("--brightness")
-  
+  // lord forgive me for what i'm about to do
+    setTimeout(() => {
     if (brightness.includes("dark")) {
-    var style = iframe.contentDocument.createElement("style")
-    iframe.setAttribute("allowTransparency", "true")
-    style.innerHTML = iframeCSS
-    console.log(iframe.contentDocument.head)
+      var style = iframe.contentDocument.createElement("style")
+      iframe.setAttribute("allowTransparency", "true")
+      style.innerHTML = iframeCSS
+      console.log(iframe.contentDocument.documentElement)
 
-    iframe.contentDocument.head.appendChild(style)
-  } else {
-    console.log("theme does not have dark brightness")
-  }
+      iframe.contentDocument.documentElement.appendChild(style)
+    } else {
+      console.log("theme does not have dark brightness")
+    }
+  }, 500);
 })
