d60f8ccf23d21b4e6454cd57c557596caa152512
aaroluo <aaro.luo@gmail.com>
2022-11-18 13:32:28 +0200

    conditionally inject iframe

--- iframe.js
+++ iframe.js
@@ -9,11 +9,18 @@ console.log("run inject");
 document.querySelectorAll("iframe").forEach(iframe => {
 
   console.log("inject iframe");
+  const ws = document.getElementById("WilmaStyles")
+  let brightness =
+    getComputedStyle(ws).getPropertyValue("--brightness")
+  
+    if (brightness.includes("dark")) {
+    var style = iframe.contentDocument.createElement("style")
+    iframe.setAttribute("allowTransparency", "true")
+    style.innerHTML = iframeCSS
+    console.log(iframe.contentDocument.head)
 
-  var style = iframe.contentDocument.createElement("style")
-  iframe.setAttribute("allowTransparency", "true")
-  style.innerHTML = iframeCSS
-  console.log(iframe.contentDocument.head)
-
-  iframe.contentDocument.head.appendChild(style)
+    iframe.contentDocument.head.appendChild(style)
+  } else {
+    console.log("theme does not have dark brightness")
+  }
 })
